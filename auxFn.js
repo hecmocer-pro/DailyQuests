bea0eff80102bf37514bcf81292cc301b346118e
Hector <hector.morenocervera@eu.jll.com>
2021-06-16 21:45:30 +0200

    added the track of history

--- auxFn.js
+++ auxFn.js
@@ -3,6 +3,8 @@ const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 const _MS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 const now = new Date()
+const historyRecord = JSON.parse(localStorage.getItem('DQ_historyRecord')) || [];
+console.log(historyRecord)
 
 const dateDiffInHours = (a, b) => {
     // Discard the time and time-zone information.
@@ -29,8 +31,8 @@ const dateDiffInWeeks = (a, b) => {
 }
 
 const setItemInLocalStorage = (id, value) => {
-    localStorage.setItem(id, value);
-    localStorage.setItem(`${id}-date`, new Date().toString());
+    localStorage.setItem(`DQ_${id}`, value);
+    localStorage.setItem(`DQ_${id}-date`, new Date().toString());
 }
 
 const setItemInHtml = (id, value) => {
@@ -53,12 +55,76 @@ const consolidateValue = (id, value) => {
     setItemInHtml(id, value)
 }
 
+const sumOneToHistoryRecord = (id) => {
+    const historyRecord = JSON.parse(localStorage.getItem('DQ_historyRecord')) || [];
+    const dateToday = getDateNoTimeFromDate(new Date())
+    const foundHistoryRecordIndex = historyRecord.findIndex(a => getDateNoTimeFromDate(a.date) === dateToday)
+    if (foundHistoryRecordIndex >= 0) {
+        const historyRecordToday = historyRecord[foundHistoryRecordIndex]
+        historyRecordToday[id] = (historyRecordToday[id] || 0) + 1
+        historyRecord[foundHistoryRecordIndex] = historyRecordToday
+    } else {
+        const historyRecordToday = {
+            date: dateToday,
+        }
+        historyRecordToday[id] = 1
+        historyRecord.push(historyRecordToday)
+    }
+    localStorage.setItem('DQ_historyRecord', JSON.stringify(historyRecord));
+}
+
+const subtractOneToHistoryRecord = (id) => {
+    const historyRecord = JSON.parse(localStorage.getItem('DQ_historyRecord')) || [];
+    const dateToday = getDateNoTimeFromDate(new Date())
+    const foundHistoryRecordIndex = historyRecord.findIndex(a => getDateNoTimeFromDate(a.date) === dateToday)
+    if (foundHistoryRecordIndex >= 0) {
+        const historyRecordToday = historyRecord[foundHistoryRecordIndex]
+        historyRecordToday[id] = (historyRecordToday[id] || 0) - 1
+        historyRecord[foundHistoryRecordIndex] = historyRecordToday
+    } else {
+        const historyRecordToday = {
+            date: dateToday,
+        }
+        historyRecordToday[id] = 0
+        historyRecord.push(historyRecordToday)
+    }
+    localStorage.setItem('DQ_historyRecord', JSON.stringify(historyRecord));
+}
+
+const cleanTodaysHistoryRecord = () => {
+    const historyRecord = JSON.parse(localStorage.getItem('DQ_historyRecord')) || [];
+    const dateToday = getDateNoTimeFromDate(new Date())
+    const foundHistoryRecordIndex = historyRecord.findIndex(a => getDateNoTimeFromDate(a.date) === dateToday)
+    if (foundHistoryRecordIndex >= 0) {
+        historyRecord[foundHistoryRecordIndex] = {
+            date: dateToday,
+        }
+        localStorage.setItem('DQ_historyRecord', JSON.stringify(historyRecord));
+    }
+}
+
+
+const join = (t, a, s) => {
+    function format(m) {
+        let f = new Intl.DateTimeFormat('en', m);
+        return f.format(t);
+    }
+    return a.map(format).join(s);
+}
+
+const getDateNoTimeFromDate = (date) => {
+    const dateToUse = new Date(date)
+    let dateFormat = [{day: 'numeric'}, {month: 'short'}, {year: 'numeric'}];
+    let dateFormatted = join(dateToUse, dateFormat, '-');
+    return dateFormatted
+}
+
 export default {
 
     getDailyQuest(id) {
-        let value = Number(localStorage.getItem(id))
+        let value = Number(localStorage.getItem(`DQ_${id}`))
 
-        const lastSavedDate = new Date(localStorage.getItem(`${id}-date`))
+        const lastSavedDate = new Date(localStorage.getItem(`DQ_${id}-date`))
 
         const daysBetween = dateDiffInDays(lastSavedDate, now)
         value += daysBetween
@@ -69,10 +135,10 @@ export default {
     },
 
     getMTGAQuest(id) {
-        let value = Number(localStorage.getItem(id))
+        let value = Number(localStorage.getItem(`DQ_${id}`))
 
         const resetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), '11', '00')
-        const lastSavedDate = new Date(localStorage.getItem(`${id}-date`))
+        const lastSavedDate = new Date(localStorage.getItem(`DQ_${id}-date`))
 
         const hoursBetweenResetAndLastSavedDate = dateDiffInHours(resetTime, lastSavedDate)
         const hoursBetweenResetAndNow = dateDiffInHours(resetTime, now)
@@ -89,9 +155,9 @@ export default {
     },
 
     getWeeklyQuest(id) {
-        let value = Number(localStorage.getItem(id))
+        let value = Number(localStorage.getItem(`DQ_${id}`))
 
-        let lastSavedDate = new Date(localStorage.getItem(`${id}-date`))
+        let lastSavedDate = new Date(localStorage.getItem(`DQ_${id}-date`))
         lastSavedDate.setDate(lastSavedDate.getDate() - 1); // para que las semanas empiecen en lunes y no en domingo
 
         const weeksBetween = dateDiffInWeeks(lastSavedDate, now)
@@ -104,6 +170,9 @@ export default {
         return value
     },
 
-    consolidateValue
+    consolidateValue,
+    sumOneToHistoryRecord,
+    subtractOneToHistoryRecord,
+    cleanTodaysHistoryRecord,
 
 }
